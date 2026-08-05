3032ec78d029efb3199ae8c24ee75d59bec9d052
luthfyaatika-stack <luthfyaatika@smpn5po.sch.id>
2026-08-05 09:06:17 +0700

    penambahan produk jsx

--- src/pages/Produk.jsx
+++ src/pages/Produk.jsx
@@ -1,4 +1,23 @@
 // Produk.jsx
-export default function Produk() {
-    return <h2>Halaman Produk</h2>;
+import { useEffect, useState } from "react";
+
+export default function Produk () {
+    const [produk, setProduk] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getProduk = async () => {
+        try {
+            const res = await fetch("http://localhost:5000/produk");
+            const data = await res.json();
+            setProduk(data);
+        } catch (err) {
+            console.error("Gagal fetch data:", err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getProduk();
+    }, []);
 }

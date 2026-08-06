00787339dfc5f7cb178a146bfbda31e552896f0a
luthfyaatika-stack <luthfyaatika@smpn5po.sch.id>
2026-08-06 11:05:16 +0700

    upudatte app

--- src/pages/AddProduk.jsx
+++ src/pages/AddProduk.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function AddProduk() {
-    const [fromData, setFromData] = useNavigate({
+    const [formData, setFormData] = useState({
         judul: "",
         deskripsi: "",
         harga: "",
@@ -12,7 +12,7 @@ export default function AddProduk() {
     const navigate = useNavigate();
 
     const handleChange = (e) => {
-        setFromData({ ...fromData, [e.target.name]: e.target.value });
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = async (e) => {
@@ -21,7 +21,7 @@ export default function AddProduk() {
             const res = await fetch("http://localhost:3001/produk", {
                 method: "POST",
                 headers: {"Content-Type": "application/json" },
-                body: JSON.stringify(FormData),
+                body: JSON.stringify(formData),
             });
             if (res.ok) {
                 alert("Produk berhasil ditambahkan 👌");
@@ -39,14 +39,15 @@ export default function AddProduk() {
     return (
         <div className="container mt-4">
             <h2 className="mb-3">Tambah Produk👀</h2>
-            <from onSubmit={handleSumbit} className="card p-4 shadow-sm">
+            <form onSubmit={handleSubmit} className="card p-4 shadow-sm">
                 <div className="mb-3">
-                    <label className="from-label">Judul Produk</label>
+                    <label className="form-label">Judul Produk</label>
                     <input
                     type="text"
                     name="judul"
-                    value={FormData.judul}
+                    value={formData.judul}
                     onChange={handleChange}
+                    className="form-control"
                     placeholder="Masukkan nama produk"
                     required
                     />
@@ -56,7 +57,7 @@ export default function AddProduk() {
                     <label className="form-label">Deskripsi</label>
                     <textarea
                     name="deskripsi"
-                    value={fromData.deskripsi}
+                    value={formData.deskripsi}
                     onChange={handleChange}
                     className="form-control"
                     placeholder="Masukkan deskripsi produk"
@@ -68,22 +69,22 @@ export default function AddProduk() {
                     <input 
                     type="number"
                     name="harga"
-                    value={FormData.harga}
+                    value={formData.harga}
                     onChange={handleChange}
-                    className="from-control"
+                    className="form-control"
                     placeholder="Masukkan harga"
                     required
                 />
                 </div>
 
                 <div className="mb-3">
-                    <label className="form-label">ID kategori</label>
+                    <label className="form-label">ID Kategori</label>
                     <input
                        type="number"
                        name="id_kategori"
-                       value={FormData.id_kategori}
-                       onCanPlay={handleChange}
-                       className="from-control"
+                       value={formData.id_kategori}
+                       onChange={handleChange}
+                       className="form-control"
                        placeholder="Masukkan ID kategori"
                        />
                 </div>
@@ -91,7 +92,7 @@ export default function AddProduk() {
                 <button type="submit" className="btn btn-success">
                     Simpan
                 </button>
-            </from>
+            </form>
         </div>
     )
 };
--- src/pages/Produk.jsx
+++ src/pages/Produk.jsx
@@ -1,5 +1,6 @@
 // Produk.jsx
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
 
 export default function Produk () {
     const [produk, setProduk] = useState([]);
@@ -31,7 +32,7 @@ export default function Produk () {
                 <h2>Daftar Produk GlowList💅</h2>
                 <Link to="/produk/tambah" className="btn btn-primary">
                     + Tambah Produk
-                    </Link>
+                </Link>
             </div>
 
             <table className="table table-bordered table-striped">
